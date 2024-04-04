d77106804cc0925ef2940f3fabc8af73ad021afc
Askar Nurbekov <nurbekoff@gmail.com>
2024-04-04 02:59:10 +0000

    my first program

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+
+#include <iostream> // Include the I/O library
+
+int main() { // The main function
+    std::cout << "Hello, World!"; // Output "Hello, World!" to the console
+    return 0; // Return 0 to indicate successful completion
+}
